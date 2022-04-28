d4098d79d73ca0948bb51381654ed8822a691a1b
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-04-28 23:06:04 +0300

    Fixed a test

--- tests/pytest/test_min_halls_scheduling.py
+++ tests/pytest/test_min_halls_scheduling.py
@@ -3,7 +3,7 @@ from minimum_halls_scheduling import *
 
 
 @pytest.mark.parametrize(
-    ("n", "data", "expectedIndices"),
+    ("n", "data", "expected"),
     (
             (5, [
                 "12:00 13:00",
@@ -12,30 +12,25 @@ from minimum_halls_scheduling import *
                 "19:00 20:00",
                 "15:40 17:30"
             ], [[2, 4, 3], [0, 1]]),
-            (1, ["15:00 13:00"], []),
-            (1, ["15:00sd 13:00"], []),
-            (1, ["a b c"], [])
+            (1, ["15:00 13:00"],
+             "Error: Invalid input, must be valid interval"),
+            (1, ["15:00sd 13:00"],
+             "Error: Invalid input, must match pattern"),
+            (1, ["a b c"],
+             "Error: Invalid input, must match pattern")
     )
 )
-def test_min_halls_scheduling(n: int, data: list[str], expectedIndices: list[int]):
+def test_min_halls_scheduling(n: int,
+                              data: list[str],
+                              expected: list[int] or str):
     result = minimum_halls_scheduling(n, data)
-    pattern = re.compile('[0-9]?[0-9]:[0-9][0-9]')
-
-    for d in data:
-        d = d.split()
-        start = d[0]
-        end = d[1]
-        if not (pattern.fullmatch(start) and pattern.fullmatch(end)):
-            assert result == "Error: Invalid input, must match pattern"
-            return
-        elif start > end:
-            assert result == "Error: Invalid input, must be valid interval"
-            return
-
-    indices = []
-    for res in result:
-        lst = []
-        for show in res:
-            lst.append(show[0])
-        indices.append(lst)
-    assert sorted(indices) == sorted(expectedIndices)
+    if type(result) == str:
+        assert result == expected
+    else:
+        indices = []
+        for res in result:
+            lst = []
+            for show in res:
+                lst.append(show[0])
+            indices.append(lst)
+        assert sorted(indices) == sorted(expected)
