7c63735dd57dddff5e7ebb36c9df31191fd32c50
Mira Stroie <miramaria27@yahoo.com>
2022-04-29 01:40:30 +0300

    Update champagne tower's source & tests.

--- champagne_tower.py
+++ champagne_tower.py
@@ -2,11 +2,11 @@ def champagneTower(poured: int, query_row: int, query_glass: int) -> float:
     if poured < 0 or query_row < 0 or query_glass < 0:
         return -1
     if query_row >= 100 or query_glass >= 100:
-        return -1
+        return -2
     if query_glass > query_row:
-        return -1
+        return -3
 
-    dp = [[0] * k for k in range(1, 102)]
+    dp = [[0] * k for k in range(1, 101)]
     dp[0][0] = poured
 
     for r in range(query_row + 1):
--- tests/pytest/test_champagne_tower.py
+++ tests/pytest/test_champagne_tower.py
@@ -5,10 +5,15 @@ from champagne_tower import *
 @pytest.mark.parametrize(
     ("poured", "query_row", "query_glass", "expected"),
     (
-            (25, 6, 1, 0.18750),
-            (-1, 0, 0, -1),
-            (1, 123, 12, -1),
-            (2, 23, 56, -1)
+           (25, 6, 1, 0.18750),
+            (-2, 0, 0, -1),
+            (1, 123, 12, -2),
+            (2, 23, 56, -3),
+            (0, 2, 1, 0),
+            (4, 0, 0, 1),
+            (4, 100, 30, -2),
+            (4, 20, 100, -2),
+            (3, 98, 98, 0)
     )
 )
 def test_champagne_tower(poured: int, query_row: int, query_glass: int, expected: float):
